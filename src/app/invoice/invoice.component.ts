211c11e5fea58f7c7247c3f806be7fa5ced69ee0
Akaaljot singh <akaljot@ventures.sas>
2019-06-26 13:40:16 +0530

    invoice chagnes

--- src/app/invoice/invoice.component.ts
+++ src/app/invoice/invoice.component.ts
@@ -1,15 +1,88 @@
 import { Component, OnInit } from '@angular/core';
 
+interface List {
+  sno: number;
+  orderno: number;
+  desc: Container[];
+  orderNumber: number;
+  price: number;
+}
+
+interface Container {
+  container: string;
+  price: number;
+}
+
+const LISTS: List[] = [
+  {
+    sno: 1,
+    orderno: 123,
+    desc: [
+      {
+        container: 'container 1',
+        price: 400
+      },
+      {
+        container: 'container 2',
+        price: 600
+      },
+      {
+        container: 'container 3',
+        price: 1000
+      }
+    ],
+    orderNumber: 200,
+    price: 2000
+  },
+  {
+    sno: 2,
+    orderno: 123,
+    desc: [
+      {
+        container: 'container 1',
+        price: 400
+      },
+      {
+        container: 'container 2',
+        price: 600
+      },
+      {
+        container: 'container 3',
+        price: 1000
+      }
+    ],
+    orderNumber: 200,
+    price: 2000
+  },
+  {
+    sno: 3,
+    orderno: 123,
+    desc: [
+      {
+        container: 'container 1',
+        price: 400
+      },
+      {
+        container: 'container 2',
+        price: 600
+      },
+      {
+        container: 'container 3',
+        price: 1000
+      }
+    ],
+    orderNumber: 200,
+    price: 2000
+  },
+];
+
 @Component({
   selector: 'app-invoice',
   templateUrl: './invoice.component.html',
   styleUrls: ['./invoice.component.scss']
 })
-export class InvoiceComponent implements OnInit {
+export class InvoiceComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  lists = LISTS;
 
 }
